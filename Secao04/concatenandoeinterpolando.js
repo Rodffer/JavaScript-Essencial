a315b7b189cc7dfa1b9e2f4e1f7ae6ca6644adb5
Rayron Rodffer <r.rodffer@gmail.com>
2021-04-24 16:28:09 -0300

    concatenando/interpolando

---
+++ Secao04/concatenandoeinterpolando.js
@@ -0,0 +1,17 @@
+// Concatenando e Interpolando
+
+let idade, nome
+
+// atribuicao de valor
+
+nome = 'Joao'
+idade = 15;
+
+
+//Concatenando
+console.log('O ' + nome + ' tem ' + idade + ' anos');
+
+//Interpolando
+console.log(`O ${nome} tem ${idade} anos`);
+
+
