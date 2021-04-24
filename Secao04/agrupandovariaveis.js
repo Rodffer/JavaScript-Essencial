17004502fe3105385d919aea4c071e0d4cfc7406
Rayron Rodffer <r.rodffer@gmail.com>
2021-04-24 16:13:16 -0300

    aula 11

---
+++ Secao04/agrupandovariaveis.js
@@ -0,0 +1,15 @@
+// Agrupar as declaracoes das variaveis
+
+// declaracao
+
+let exemplo1, exemplo2, exemplo3
+
+// atribuicao de valor
+
+exemplo1 = 2021;
+exemplo2 = 'Name'
+exemplo3 = true;
+
+// que tipo de dado essa variavel usa
+
+console.log(exemplo1, exemplo2, exemplo3);
--- Secao04/declaracaoeatribuicao.js
+++ Secao04/declaracaoeatribuicao.js
@@ -4,13 +4,16 @@
 
 let exemplo1
 let exemplo2
+let exemplo3
 
 // atribuicao de valor
 
 exemplo1 = 2021;
 exemplo2 = 'Name'
+exemplo3 = true;
 
 // que tipo de dado essa variavel usa
 
 console.log(typeof exemplo1);
 console.log(typeof exemplo2);
+console.log(typeof exemplo3);
