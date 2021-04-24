d5dd15f2b65a1a316c8ae8f1710186bfb95fa769
Rayron Rodffer <r.rodffer@gmail.com>
2021-04-24 15:52:22 -0300

    aula 09

---
+++ Secao04/nomeandovariaveis.js
@@ -0,0 +1,35 @@
+// Declarando variaveis
+
+    // case-sensitive (sensivel ao caso) A a
+    // Unicode pode colocar  _
+
+// EXEMPLOS
+
+    //PODE FAZER 
+    // iniciar variavel com _
+    // iniciar variavel com letras
+    // difencia maiuscalas de minusculas
+
+    //NAO PODE FAZER
+    //iniciar com numeros
+    //usar espacos
+
+    //IDEAL
+    // colocar nomes descritivos
+    // camelCase
+    // snake_case
+    // declarar em ingles
+
+
+//EXEMPLOS PRATICOS
+    // NAO PODE FAZER
+
+    // const 3nome = 'Nome'; // nao inicie variaveis com numeros
+    // const primeiro nome = 'Nome'; // nao utilize espacos nas variaveis
+
+
+    //IDEAL
+const firstName = 'Joao';
+const secondName = 'Silva';
+
+console.log(firstName, secondName);
