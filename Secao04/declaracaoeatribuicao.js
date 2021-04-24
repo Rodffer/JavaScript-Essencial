f9afa21c5c7b4e028c6fe4eb22edb6766cd992af
Rayron Rodffer <r.rodffer@gmail.com>
2021-04-24 16:03:27 -0300

    usando typeof para pegar o tipo da variavel

---
+++ Secao04/declaracaoeatribuicao.js
@@ -0,0 +1,16 @@
+// Variaveis e tipos de dados
+
+// declaracao
+
+let exemplo1
+let exemplo2
+
+// atribuicao de valor
+
+exemplo1 = 2021;
+exemplo2 = 'Name'
+
+// que tipo de dado essa variavel usa
+
+console.log(typeof exemplo1);
+console.log(typeof exemplo2);
