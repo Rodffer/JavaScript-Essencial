d5d6d3367925b778a6d90ad773a22cf78d0d6ac4
Rayron Rodffer <r.rodffer@gmail.com>
2021-07-24 23:40:48 -0300

    maiúsculas e minúsculas

---
+++ Secao06/maiusculasEminusculas.js
@@ -0,0 +1,8 @@
+// transformando maiusculas e minusculas
+
+let exemplo1 = 'EmailTesteJavaScript@hotmail.com'
+
+console.log('Minusculas ->', exemplo1.toLocaleLowerCase())
+
+
+console.log('Maiusculas ->', exemplo1.toLocaleUpperCase())
