b17e8c44c28fd9e77897aa60d2193bf26bcd4d2f
Rayron Rodffer <r.rodffer@gmail.com>
2021-07-24 23:16:30 -0300

    contando caracteres

---
+++ Secao06/contandoCaracteres.js
@@ -0,0 +1,13 @@
+// contar caracteres
+
+let palavra = 'Computador'
+let listaDePalavras = ['Palavra1', 'Palavra2', 'Palavra3']
+
+console.log(palavra.length)
+
+console.log(listaDePalavras.length)
+
+let numero1 = 10
+
+console.log(String(numero1).length)
+
