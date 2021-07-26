bf0b43d12e47d869795d70f596743a29da74fe79
Rayron Rodffer <r.rodffer@gmail.com>
2021-07-25 21:17:52 -0300

    operadores matematicos e operadores logicos

---
+++ Secao07/operacoesMatematicasEOperadoresLogicos.js
@@ -0,0 +1,32 @@
+// ------------ Operações matemáticas e Operadores lógicos  -------------//
+
+// OPERADORES MATEMATICOS
+// + - * / % 
+
+// OPERADORES LOGICOS
+//= == === ! < > <= >= || &&
+
+/*
+  AGRUPAMENTO                 ()
+  NEGACAO E INCREMENTO        ! ++ --
+  MULTIPLICACAO E ADICAO      * /
+  ADICAO E SUBTRACAO          + -
+  RELACIONAL                  < <= > >=
+  IGUALDADE                   = == === != !==
+  E                           &&
+  OU                          ||
+  CONDICIONAL                 ?:
+  ATRIBUICAO                  = += -= *=
+*/
+
+let a = 10, b = 5
+
+let soma = (a + b)
+let subtrair = (a - b)
+let multiplicar = (a * b)
+let dividir = (a / b)
+
+console.log('Soma', soma)
+console.log('Subtracao',subtrair)
+console.log('Multiplicacao',multiplicar)
+console.log('Divisao',dividir)
