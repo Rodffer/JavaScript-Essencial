86c8c8ee8d11956c7591bb46475f46d7c497f485
Rayron Rodffer <r.rodffer@gmail.com>
2021-07-25 21:59:06 -0300

    condicional ternario

---
+++ Secao07/condicionalTernario.js
@@ -0,0 +1,16 @@
+//--------------- Operador Condicional Ternário-----------------//
+
+let numero1 = 25
+let numero2 = 15
+
+// Primeiro valor IF segungo Else
+let resultado = numero1 > numero2 ? 'Primeiro numero maior' : 'Segundo numero Maior'
+console.log(resultado)
+
+console.log(numero1 > numero2)
+
+let idadePessoa = 15
+
+// Primeiro valor IF segungo Else
+let resultadoIdade = idadePessoa >= 18 ? 'Pessoa maior de 18 anos' : 'Pessoa menor de idade' 
+console.log(resultadoIdade)
